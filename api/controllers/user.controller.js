fdc3ff26fcb2b2c2645f63948ae3536f99619767
cdxgh <khanarshu0786@gmail.com>
2024-06-05 12:20:48 +0530

    API Handling

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,5 @@
+export const test = (req,res)=>{
+
+    res.send("Reqquest");
+};
+
--- api/index.js
+++ api/index.js
@@ -1,16 +1,33 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
 dotenv.config();
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose.connect(process.env.MONGO).then(() => {
     console.log("MongoDB Connected!");
-}).catch((err)=>{
+}).catch((err) => {
     console.log("Error:" + err);
 });
 
 
 const app = express();
+app.use(express.json());
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("server is running at port 3000!");
+});
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
 });
