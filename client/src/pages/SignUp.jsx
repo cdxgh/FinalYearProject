5d3a650c9a27f795c18d959d276998ce31e11730
cdxgh <khanarshu0786@gmail.com>
2024-07-18 11:49:32 +0530

    changes in clientside

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: ''
+  });
 
-export default SignUp
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!data.success) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      // On successful sign-up, navigate to the sign-in page
+      navigate('/sign-in');
+    } catch (error) {
+      setError('An error occurred. Please try again.');
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign-Up</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          type='text'
+          placeholder='Username'
+          className='border p-3 rounded-lg'
+          id='username'
+          value={formData.username}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type='email'
+          placeholder='Email'
+          className='border p-3 rounded-lg'
+          id='email'
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          className='border p-3 rounded-lg'
+          id='password'
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+        <button
+          type='submit'
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+          disabled={loading}
+        >
+          {loading ? 'Loading.....' : 'Sign Up'}
+        </button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Already have an account?</p>
+        <Link to='/sign-in'>
+          <span className='text-blue-700'>Sign-In</span>
+        </Link>
+      </div>
+      {error && <p className='text-red-500 mt-3'>{error}</p>}
+    </div>
+  );
+};
+
+export default SignUp;
